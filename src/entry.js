2bf652ba140b4e26a8cada820b89a9d531c9e7be
Hayato Yamauchi <yamauchi@r3it.com>
2017-11-30 00:53:58 +0900

    子コンポーネントのイベントをハンドルする

--- src/entry.js
+++ src/entry.js
@@ -17,6 +17,13 @@ import VueDatepickerLocal from "vue-datepicker-local";
             monthsHead: "1月_2月_3月_4月_5月_6月_7月_8月_9月_10月_11月_12月".split("_"), // months of head
             months: "1月_2月_3月_4月_5月_6月_7月_8月_9月_10月_11月_12月".split("_") // months of panel
           }
+        },
+        on: {
+          input: e => {
+            const item = kintone.app.record.get();
+            item["record"]["text1"]["value"] = `${e.getFullYear()}-${("0" + (e.getMonth() + 1)).slice(-2)}`;
+            kintone.app.record.set(item);
+          }
         }
       })
     });
