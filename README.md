750d8fe8e9d03ee88682432720a0cb652b1799ae
Hayato Yamauchi <yamauchi@r3it.com>
2017-11-30 00:10:19 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# kintone-vue-datepicker-local-sample1
+
+## これはなに？ / What's this?
+
---
+++ src/entry.js
@@ -0,0 +1,25 @@
+import Vue from "vue";
+import VueDatepickerLocal from "vue-datepicker-local";
+
+(function () {
+  kintone.events.on("app.record.create.show", (event) => {
+    const elm = kintone.app.record.getSpaceElement("space1");
+    if (!elm) {
+      return event;
+    }
+    const vm = new Vue({
+      el: elm,
+      render: h => h(VueDatepickerLocal, {
+        props: {
+          format: "YYYY-MM",
+          local: {
+            yearSuffix: "年", // format of head
+            monthsHead: "1月_2月_3月_4月_5月_6月_7月_8月_9月_10月_11月_12月".split("_"), // months of head
+            months: "1月_2月_3月_4月_5月_6月_7月_8月_9月_10月_11月_12月".split("_") // months of panel
+          }
+        }
+      })
+    });
+    return event;
+  });
+})();
